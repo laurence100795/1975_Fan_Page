679158f1afa8926d9c8142e7e84b6697f3916982
laurence100795 <laurence100795@gmail.com>
2020-02-23 17:29:00 +0000

    footer changes JS animation added to homepage (requires optimisation slow rendering)

--- Assets/JS/HomeJS.js
+++ Assets/JS/HomeJS.js
@@ -1,14 +1,18 @@
-$(document).ready(function () {
-    $(function () {
-        $(window).resize(function () {
-            if($("#").width() <= 767) {
-                $('#ArrowUp').addClass('fa-arrow-up');
-                $('#ArrowUp').removeClass('fa-arrow-left');
-            } else {
-                $('#ArrowUp').removeClass('fa-arrow-up');
-                $('#ArrowUp').addClass('fa-arrow-left');
-            }
-        });
-    });
+/*BackGround Slider Animation*/
 
-});
+const slideshowImages = document.querySelectorAll(".SlideImage");
+
+const nextSlideDelay = 5000;
+let currentSlideCounter = 0; 
+
+slideshowImages[currentSlideCounter].style.opacity = 1;
+
+setInterval(nextSlide, nextSlideDelay);
+
+function nextSlide() {
+    slideshowImages[currentSlideCounter].style.opacity= 0;
+    currentSlideCounter = (currentSlideCounter + 1) % slideshowImages.length;
+    slideshowImages[currentSlideCounter].style.opacity = 1;
+}
+
+/*BackGround Slider Animation*/
